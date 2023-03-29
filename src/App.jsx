d71af9757a16c7df83f5ac24405a089a428f7ecf
Scott Jones <itspinky95@hotmail.com>
2023-03-29 13:59:13 +0100

    Added spotify to page

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Work from './components/Work'
 import About from './components/About'
 import Portfolio from './components/Portfolio.jsx'
 import Contact from './components/Contact'
+import Spotify from './components/Spotify'
 
 
 function App() {
 
   return (
     <div>
+      <Spotify/>
       <Sidenav/>
       <Main/>
       <About/>
---
+++ src/components/Spotify.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Spotify = () => {
+
+    return (
+        <div className='absolute top-0 right-0 border-emerald-600'>
+            <iframe src="https://open.spotify.com/embed/playlist/7M3lwAY5fVpXQX84RjCt2h?utm_source=generator" width='350' height="250" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </div>
+    )
+}
+
+export default Spotify

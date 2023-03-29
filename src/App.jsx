1777fc9677ab02d14bfdab5ab8d6b2db19dcf07e
Scott Jones <itspinky95@hotmail.com>
2023-03-29 14:16:45 +0100

    Revert "Added spotify to page"
    This reverts commit d71af9757a16c7df83f5ac24405a089a428f7ecf.

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,12 @@ import Work from './components/Work'
 import About from './components/About'
 import Portfolio from './components/Portfolio.jsx'
 import Contact from './components/Contact'
-import Spotify from './components/Spotify'
 
 
 function App() {
 
   return (
     <div>
-      <Spotify/>
       <Sidenav/>
       <Main/>
       <About/>
